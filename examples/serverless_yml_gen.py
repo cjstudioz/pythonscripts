294be7951220ca5534e78c9a3353355d00338c0b
cjstudioz <cjstudioz@users.noreply.github.com>
2019-08-30 09:05:36 +0100

    Update serverless_yml_gen.py

--- examples/serverless_yml_gen.py
+++ examples/serverless_yml_gen.py
@@ -1,16 +1,32 @@
-
+import importlib.util
 import os
 import glob
 import yaml
 
-my_path = r'lambdas' #r'lambdas'
-files = glob.glob(my_path + '/**/*.py', recursive=True)
-handlers = [file.replace(os.path.sep, '.').replace('.py', '.default') for file in files if '__init__' not in file]
+#inputs
+lambda_root = r'lambdas'
 
 
+def load_module(path):
+    module_name = file.replace(os.path.sep, '.').replace('.py', '')
+    spec = importlib.util.spec_from_file_location(module_name, path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
+
+paths = glob.glob(lambda_root + '/**/*.py', recursive=True)
+modules = [load_module(path) for path in paths if '__init__' not in path]
+
+result = {}
+for module in modules:
+    path = module.__name__
+    name = path.replace('.', '-').replace(lambda_root, '')[1:]
+    url = name.replace('-', '/')
+    overrides = module.AWS_LAMBDA_CFG
+    
 
 result = {
-	'functions': {		
+	'functions': {
 		file: {
 		    'handler': file,
 		    'layers': '',
@@ -24,7 +40,7 @@ result = {
 			    },
 			],
 		}
-	for file in handlers}
+	}
 }
 yamlstr = yaml.safe_dump(result)
 print(yamlstr)
