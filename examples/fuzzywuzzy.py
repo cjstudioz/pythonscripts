6e293668223d078ce9d6704625de3b63dc69e074
cjstudioz <cjstudioz@users.noreply.github.com>
2019-11-12 08:56:24 +0000

    Update fuzzywuzzy.py

--- examples/fuzzywuzzy.py
+++ examples/fuzzywuzzy.py
@@ -30,3 +30,46 @@ for cluster_id in np.unique(affprop.labels_):
     cluster = np.unique(words[np.nonzero(affprop.labels_==cluster_id)])
     cluster_str = ", ".join(cluster)
     print(" - *%s:* %s" % (exemplar, cluster_str))
+    
+###################################################################################
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 08:34:23 2019
+
+@author: c-ChunL
+"""
+
+import numpy as np
+import sklearn.cluster
+from fuzzywuzzy import fuzz, process
+import nltk
+from collections import defaultdict
+
+words = [
+    'my cat is black',        
+    'my cat is white',        
+    'my dog is black',
+    'alan aint black',
+    'jane aint blue', 
+    'blue is jane',       
+]
+
+threshold = 60
+result = defaultdict(list)
+
+while words:
+    remainder = []
+    firstWord = words[0]
+    distances = [(fuzz.ratio(firstWord, word), word) for word in words]
+    for w in distances:
+        if w[0] > threshold:
+            result[firstWord].append(w[1])
+        else:
+            remainder.append(w[1]) 
+    words = remainder        
+    
+    
+sent = "Io andiamo to the beach with my amico."
+" ".join(w for w in nltk.wordpunct_tokenize(sent) \
+         if w.lower() in words or not w.isalpha())
